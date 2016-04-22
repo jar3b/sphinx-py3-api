f79fd1b761725af54a10c60a42f42e6b9ac3a4ef
jar3b <hellotan@live.ru>
2016-04-22 13:51:32 +0300

    Add correct version (2.3.1)

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ##Installation
 `pip install https://github.com/jar3b/sphinx-py3-api/zipball/master`
 ##Version
- - Sphinx: 2.2.10
+ - Sphinx: 2.3.1-beta
  - Api: sphinxapi.py 4885 2015-01-20 07:02:07Z deogar
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='sphinx-py3-api',
-    version='0.0.1',
+    version='2.3.1',
     packages=['sphinxapi'],
     url='https://github.com/jar3b/sphinx-py3-api',
     license='MIT',
