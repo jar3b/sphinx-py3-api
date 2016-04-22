a3581f9aa9f52db0bfc3ec03f01bed5b96e1548a
jar3b <hellotan@live.ru>
2016-04-22 12:20:36 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#sphinx-py3-api
+
+##Installation
+`pip install https://github.com/jar3b/sphinx-py3-api/zipball/master`
+##Version
+ - Sphinx: 2.2.10
+ - Api: sphinxapi.py 4885 2015-01-20 07:02:07Z deogar
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sphinx-py3-api',
+    version='0.0.1',
+    packages=['sphinxapi'],
+    url='https://github.com/jar3b/sphinx-py3-api',
+    license='MIT',
+    author='Sphinx Technologies Inc.',
+    author_email='',
+    description='Sphinxapi for python ported to py3'
+)
