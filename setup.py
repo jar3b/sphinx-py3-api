7d30fbfa47bcf52982fc8f43e3652b065721d93c
jar3b <hellotan@live.ru>
2016-04-24 13:17:51 +0300

    Add 'beta' to version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='sphinx-py3-api',
-    version='2.3.1',
+    version='2.3.1-beta',
     packages=['sphinxapi'],
     url='https://github.com/jar3b/sphinx-py3-api',
     license='MIT',
